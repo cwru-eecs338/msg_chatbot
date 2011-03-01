f89c7e38c52f524120dbb28c7a6a626df1427c1b
Gary Doran, Jr <garydoranjr@gmail.com>
2011-02-28 19:47:45 -0500

    Untested server implementation

--- common.h
+++ common.h
@@ -1,6 +1,16 @@
 #ifndef MSG_CHATBOT_COMMON
 #define MSG_CHATBOT_COMMON
 
+/*
+ * Message Structure
+ */
+#define MAX_MSG_LEN 140
+struct chat_msg {
+    long int message_type;
+    long int return_type;
+    char buffer[MAX_MSG_LEN];
+};
+
 int try(int, char*);
 
 #endif
--- server.c
+++ server.c
@@ -4,10 +4,13 @@
 #include <sys/msg.h>
 #include <assert.h>
 #include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 #include "common.h"
 
-void main_loop(void);
+void main_loop(int);
 void interrupt_handler(int);
 
 int interrupted = 0;
@@ -21,7 +24,7 @@ int main() {
         msgget(IPC_PRIVATE, IPC_CREAT | 0600),
         "Could not create message queue");
 
-    main_loop();
+    main_loop(msgqid);
 
     // Cleanup Queue
     try(
@@ -31,10 +34,43 @@ int main() {
     return EXIT_SUCCESS;
 }
 
-void main_loop() {
+void main_loop(int msgqid) {
+    pid_t pid = getpid();
+    printf("Server started with:\n\tpid: %d\n\tqid: %d\n",
+        pid, msgqid);
+
+    struct chat_msg incoming, outgoing;
+    int status;
     while(!interrupted) {
 
+        // Receive message
+        status = msgrcv(
+            msgqid,                  // Queue ID
+            (void *) &incoming,     // Place to store it
+            sizeof(struct chat_msg), // Maximum size
+            (long int) pid,          // Message type (for us)
+            0);                      // No flags
+        // If we were interrupted or
+        // or some error occurred
+        if(status < 0) { break; }
+
+        // Construct response
+        outgoing.message_type = incoming.return_type;
+        outgoing.return_type = (long int) pid;
+        strcpy(outgoing.buffer, "Fine.");
+
+        // Send message
+        status = msgsnd(
+            msgqid,                 // Queue ID
+            (void *) &outgoing,      // Message to send
+            sizeof(struct chat_msg), // Message Size
+            0);                      // No flags
+        // If we were interrupted or
+        // or some error occurred
+        if(status < 0) { break; }
+
     }
+
     puts("Server shutting down...");
 }
 
